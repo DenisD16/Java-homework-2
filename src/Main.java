e3e43d6e39c91d5ea9c6c2bf1352cf43284bde01
Den test <dmitrenkoden2012@mail.ru>
2023-04-05 21:50:23 +0300

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int tiket = 13676 // стоймость билета
-        int mile = 20 // за каждые 20 р. начисляется 1 миля
+        int tiket = 12000; // стоймость билета
+        int mile = 20; // за каждые 20 р. начисляется 1 миля
 
         System.out.println((tiket / mile) + " начисленно милей");
-
     }
+}
